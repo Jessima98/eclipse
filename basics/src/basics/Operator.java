b8ffd65743dcf060785400566a9c1b9ad38d8aad
Jessima <jessimajohn@gmail.com>
2021-04-12 10:19:29 +0530

    Commit Eclipse

---
+++ basics/src/basics/Operator.java
@@ -0,0 +1,21 @@
+package basics;
+
+public class Operator {
+
+
+	
+	static int c=20;
+
+public static void main(String[] args)
+{
+	int a=2;
+	System.out.println(a++);
+	System.out.println(a);
+	System.out.println(++a);
+	System.out.println(a);
+	System.out.println(a++);
+	System.out.println(a++);
+	
+	
+}
+}
